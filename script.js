35d0b259d32e5ca75c37f857890a47b4b4b973c7
zmuhls <zmuhlbauer1@gmail.com>
2022-10-20 18:49:28 -0400

    add image

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert('My project is going to be awesome, dude!');
+$("#noShow").hide();
+$("h1").html("The Best Jokes Ever!");
+$("#joke").append("<p>Q. What do you call a fake noodle? <br /> A. An impasta</p>");
+
+$("#toggleJokes").css({
+    "background-color": "orange",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
+
+$("#toggleJokes").click(function() {
+    $("#joke").toggle();
+  });
